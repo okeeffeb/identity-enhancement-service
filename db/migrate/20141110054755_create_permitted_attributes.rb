d38d185f46f0009b94e643bec7caa73b4401d2d6
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-11-11 09:41:56 +1000

    Add extra 'AvailableAttribute' model
    Track attributes in a more global sense, and then relate providers to
    the attributes by turning permitted_attributes into a join table rather
    than directly permitting values.

--- db/migrate/20141110054755_create_permitted_attributes.rb
+++ db/migrate/20141110054755_create_permitted_attributes.rb
@@ -2,7 +2,7 @@ class CreatePermittedAttributes < ActiveRecord::Migration
   def change
     create_table :permitted_attributes do |t|
       t.references :provider, null: false, default: nil
-      t.string :value, null: false, default: nil
+      t.references :available_attribute, null: false, default: nil
       t.timestamps
 
       t.index :provider_id
