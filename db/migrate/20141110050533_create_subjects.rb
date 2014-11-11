92060ed60117beb518cf09d481048f62e8ad7457
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-11-11 12:56:32 +1000

    Allow subjects to be 'incomplete' when created
    Subjects may be created by an administrative user of the system, and
    thus not yet be associated with an EPTID / AEPST. Need to allow these
    objects to be created.

--- db/migrate/20141110050533_create_subjects.rb
+++ db/migrate/20141110050533_create_subjects.rb
@@ -3,8 +3,9 @@ class CreateSubjects < ActiveRecord::Migration
     create_table :subjects do |t|
       t.string :name, null: false, default: nil
       t.string :mail, null: false, default: nil
-      t.string :targeted_id, null: false, default: nil
-      t.string :shared_token, null: false, default: nil
+      t.string :targeted_id, null: true, default: nil
+      t.string :shared_token, null: true, default: nil
+      t.boolean :complete, null: false, default: false
       t.timestamps
 
       t.index :targeted_id
