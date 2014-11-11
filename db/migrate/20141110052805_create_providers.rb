7e7fe579632959a07d8f3a9faa7f0c21a8eec623
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-11-11 13:06:47 +1000

    Add description column to providers table

--- db/migrate/20141110052805_create_providers.rb
+++ db/migrate/20141110052805_create_providers.rb
@@ -2,6 +2,7 @@ class CreateProviders < ActiveRecord::Migration
   def change
     create_table :providers do |t|
       t.string :name, null: false, default: nil
+      t.string :description, null: true, default: nil
       t.string :identifier, null: false, default: nil
       t.timestamps
     end
