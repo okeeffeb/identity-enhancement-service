16ee8f7c1898938344622b4d583ba88b83471585
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-11-12 07:28:15 +1000

    Add 'description' for API Subjects

--- db/migrate/20141110201924_create_api_subjects.rb
+++ db/migrate/20141110201924_create_api_subjects.rb
@@ -4,6 +4,7 @@ class CreateAPISubjects < ActiveRecord::Migration
       t.belongs_to :provider, null: false, default: nil
       t.string :x509_cn, null: false, default: nil
       t.string :name, null: false, default: nil
+      t.string :description, null: false, default: '', length: 4096
       t.string :contact_name, null: false, default: nil
       t.string :contact_mail, null: false, default: nil
       t.timestamps
