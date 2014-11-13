b4e8e0ab76573bd813ba740dbf16b529ff6ed95c
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-11-13 10:00:15 +1000

    Abbreviated service name for sessions and database

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
-redis_namespace = "identity-enhancement-service:#{Rails.env}:session"
+redis_namespace = "ide-service:#{Rails.env}:session"
 session_store_opts = {
   redis_server: "redis://127.0.0.1:6379/0/#{redis_namespace}",
   expire_in: 3600,
-  key: '_identity-enhancement-service_session'
+  key: '_ide-service_session'
 }
 
 Rails.application.config.session_store(:redis_store, session_store_opts)
