a6b3d4082d8d96881807ad7460607796ec7a66a0
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-12-03 21:11:06 +1000

    Pin permission audits to provider rather than role

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,7 +1,8 @@
 class Permission < ActiveRecord::Base
-  audited comment_required: true, associated_with: :role
+  audited comment_required: true, associated_with: :provider
 
   belongs_to :role
+  delegate :provider, to: :role
 
   validates :role, presence: true
 
