2cb8042c6c2e62462f01fc35983f3f8773e0d9ef
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-12-03 10:56:18 +1000

    Adjust invitation columns to mirror subject better
    When creating an invitation, we want to capture the name and email
    address entered by the administrator for auditing purposes. If there is
    ever a question about where an invite went and why somebody else claimed
    it, we can look here for clues.

--- db/migrate/20141110063114_create_invitations.rb
+++ db/migrate/20141110063114_create_invitations.rb
@@ -4,7 +4,8 @@ class CreateInvitations < ActiveRecord::Migration
       t.belongs_to :provider, null: false, default: nil
       t.belongs_to :subject, null: false, default: nil
       t.string :identifier, null: false, default: nil
-      t.string :email, null: false, default: nil
+      t.string :name, null: false, default: nil
+      t.string :mail, null: false, default: nil
       t.boolean :used, null: false, default: false
       t.timestamp :expires, null: false, default: nil
       t.timestamps
