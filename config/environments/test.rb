a23f4cefd8bf2e138e589a5e7d877b2d33ebd44e
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-12-06 09:20:49 +1000

    Remove stray 'Mail' reference which was erroring

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,8 +9,4 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.delivery_method = :test
   config.active_support.deprecation = :stderr
-
-  Mail.defaults do
-    delivery_method :test
-  end
 end
