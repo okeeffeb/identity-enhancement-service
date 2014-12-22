bc5fe097e0413c8c9bdb52524d15a562c83b1bb2
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2014-12-22 20:10:33 +1000

    Remove cluttering links from providers index
    They're all available from the 'show' view anyway

--- spec/feature/providers_admin_spec.rb
+++ spec/feature/providers_admin_spec.rb
@@ -49,8 +49,10 @@ RSpec.feature 'Modifying Providers', js: true do
   scenario 'editing a provider' do
     visit '/providers'
     within('table tr', text: provider.name) do
-      click_link 'Edit'
+      click_link 'View'
     end
+
+    click_link 'Edit'
 
     new_name = "New Name #{provider.name}"
 
@@ -121,8 +123,10 @@ RSpec.feature 'Modifying Providers', js: true do
       visit '/providers'
 
       within('table tr', text: provider.name) do
-        click_link 'Edit'
+        click_link 'View'
       end
+
+      click_link 'Edit'
     end
 
     given(:button) { 'Save' }
@@ -150,8 +154,10 @@ RSpec.feature 'Modifying Providers', js: true do
     expect(page).to have_css('table tr td', text: provider.name)
 
     within('table tr', text: provider.name) do
-      click_delete_button
+      click_link 'View'
     end
+
+    click_delete_button
 
     expect(current_path).to eq(providers_path)
     expect(page).to have_no_css('table tr td', text: provider.name)
