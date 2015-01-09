641894101a5e84aa8235e5a98df33e78ccf2d546
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-01-09 13:52:27 +1000

    Retrieve environment_string from configuration

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   # TODO: Replace this with actual required behaviour.
   context '#environment_string' do
+    let(:string) { Faker::Lorem.sentence }
+
+    before do
+      allow(Rails.application.config)
+        .to receive_message_chain(:ide_service, :environment_string)
+        .and_return(string)
+    end
+
     subject { helper.environment_string }
-    it { is_expected.to eq('Development') }
+    it { is_expected.to eq(string) }
   end
 end
