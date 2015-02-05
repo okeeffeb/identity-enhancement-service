a230106ed63168a88b79802adbc8cbe8273452e7
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-02-05 11:44:59 +1000

    Filter provider roles on dashboard
    Only provide roles related to web access to dashboard generation.
    
    Closes #32.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,19 @@
 class DashboardController < ApplicationController
   def index
     public_action
-    @provider_roles = @subject.roles.group_by(&:provider)
+    @provider_roles = filter_dashboard_roles
     @provided_attributes =
       @subject.provided_attributes.includes(
         permitted_attribute: [:provider, :available_attribute])
+  end
+
+  private
+
+  def filter_dashboard_roles
+    @subject.roles.select { |role| web_role?(role) }.group_by(&:provider)
+  end
+
+  def web_role?(role)
+    subject.permits?("providers:#{role.provider.id}:read")
   end
 end
